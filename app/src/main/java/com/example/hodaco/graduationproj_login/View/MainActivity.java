b2d1b0febe9888491133d9f3eae0434ab3f32eaa
hoda mohamed <eng.hodamohamed1@gmail.com>
2018-04-13 01:35:00 +0200

    change1

--- app/src/main/java/com/example/hodaco/graduationproj_login/View/MainActivity.java
+++ app/src/main/java/com/example/hodaco/graduationproj_login/View/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.hodaco.graduationproj_login.View;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Patterns;
@@ -32,8 +41,11 @@ public class MainActivity extends AppCompatActivity implements Login_Contract.vi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        presenter = new Login_Presenter(this);
+        if (!isConnect(MainActivity.this)) buildDialog(MainActivity.this).show();
+        else {
+            ButterKnife.bind(this);
+            presenter = new Login_Presenter(this);
+        }
     }
 
     @Override
@@ -50,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements Login_Contract.vi
             }
         });
     }
-
     @Override
     public String getEmail() {
         return email.getText().toString();
@@ -73,6 +84,32 @@ public class MainActivity extends AppCompatActivity implements Login_Contract.vi
             Toast.makeText(MainActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
         }
 
+    }
+    public boolean isConnect (Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting()))
+                return true;
+            else
+                return false;
+        } else
+            return false;
+    }
+    public AlertDialog.Builder buildDialog(Context c)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("You need to have Mobile Data or wifi to access this.press ok to Exit");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        return builder;
     }
 }
 //ADD767
